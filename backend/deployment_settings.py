16bcfd2942ab13f2334649beaf3c78466af54d8b
Ma-aruf Meek <ma-aruf@outlook.com>
2024-12-27 23:47:32 +0000

    updated deployment settings

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -35,7 +35,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ["DATABASE_URL"],
         conn_max_age=600
     )
 }
